009f3966926664e5ca10a8745531208d25ef8f3a
leo <leozhang621@gmail.com>
2017-07-14 15:02:43 +0800

    update weather

--- weather/weather.js
+++ weather/weather.js
@@ -9,50 +9,52 @@
 
 $(document).ready(function() {
 
-
-    //获取城市数据
-    $.get("https://jirenguapi.applinzi.com/city.php")
-     .done(function(cityData) {
-         $('#message span').text(cityData)
-     }).fail(function() {
-         alert('获取城市出错')
-    })
-
+    getCity()
 
     //获取当前日期
     let nowDay = new Date()
     $('#message>p:nth-of-type(2)').text('0' + (nowDay.getMonth() + 1) + '月' + nowDay.getDate() + '日')
 
-
-    //获取天气数据
-    $.get('https://jirenguapi.applinzi.com/weather.php')
-     .done(function(data) {
-         let weatherData =JSON.parse(data).results[0].weather_data//4个对象
-         console.log(weatherData)
-         weatherData.forEach(function(item,index) {
-             $('#main-content>li').eq(index).children().eq(0).text('0' + (nowDay.getMonth() + 1) + '月' + (nowDay.getDate() +index) + '日')
-             $('#main-content>li').eq(index).children().eq(1).text(item.temperature)
-             if(/[\u4e91]/.test(item.weather)) {
-                 $('#main-content>li').eq(index).children().eq(2).children().attr('xlink:href','#icon-icon-test1')
-             } else if(/[\u96e8]/.test(item.weather)) {
-                 $('#main-content>li').eq(index).children().eq(2).children().attr('xlink:href','#icon-icon-test4')
-             } else {
-                 $('#main-content>li').eq(index).children().eq(2).children().attr('xlink:href','#icon-sun')
-             }
-         })
-     })
-
-
-
-
-    //展示信息
-
+    getWeather()
 
 })
 
+//获取城市数据
+function getCity() {
+    let url = "https://jirenguapi.applinzi.com/city.php"
+    $.get(url)
+        .done(function(cityData) {
+            $('#message span').text(cityData)
+        }).fail(function() {
+        alert('获取城市出错')
+    })
+}
 
 
-/*http://jirenguapi.applinzi.com/city.php*/
-/*http://jirenguapi.applinzi.com/weather.php?city=北京*/
-
-/*https://api.seniverse.com/v3/weather/daily.json?key=1hnawhehgdkq4nhl&location=beijing&language=zh-Hans&unit=c&start=0&days=5*/
+//获取天气数据
+function getWeather(city) {
+    if(city) {
+        let url = 'https://jirenguapi.applinzi.com/weather.php' + '?city=' + city
+    } else {
+        let url = 'https://jirenguapi.applinzi.com/weather.php'
+    }
+    $.get(url)
+        .done(function(data) {
+            let weatherData =JSON.parse(data).results[0].weather_data  //4个天气对象
+            console.log(weatherData)
+            weatherData.forEach(function(item,index) {
+                $('#main-content>li').eq(index).children().eq(0).text('0' + (nowDay.getMonth() + 1) + '月' + (nowDay.getDate() +index) + '日')
+                $('#main-content>li').eq(index).children().eq(1).text(item.temperature)
+                $('#main-content>li').eq(index).children().eq(3).text(item.weather)
+                if(/[\u4e91]/.test(item.weather)) {
+                    $('#main-content>li').eq(index).children().eq(2).children().attr('xlink:href','#icon-icon-test1')
+                } else if(/[\u96e8]/.test(item.weather)) {
+                    $('#main-content>li').eq(index).children().eq(2).children().attr('xlink:href','#icon-icon-test4')
+                } else {
+                    $('#main-content>li').eq(index).children().eq(2).children().attr('xlink:href','#icon-sun')
+                }
+            })
+        }).fail(function() {
+        alert('获取天气失败')
+    })
+}
