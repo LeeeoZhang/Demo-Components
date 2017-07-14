18afd8e09714b58f82ffe4e1a58ab99ada5aae1a
leo <leozhang621@gmail.com>
2017-07-14 12:59:01 +0800

    update weather

--- weather/weather.js
+++ weather/weather.js
@@ -9,6 +9,7 @@
 
 $(document).ready(function() {
 
+
     //获取城市数据
     $.get("https://jirenguapi.applinzi.com/city.php")
      .done(function(cityData) {
@@ -17,16 +18,35 @@ $(document).ready(function() {
          alert('获取城市出错')
     })
 
+
     //获取当前日期
     let nowDay = new Date()
-    console.log(nowDay)
-    $('#message>p:nth-of-type(2)').text(nowDay.getMonth() + 1 + '月' + nowDay.getDate() + '日')
+    $('#message>p:nth-of-type(2)').text('0' + (nowDay.getMonth() + 1) + '月' + nowDay.getDate() + '日')
+
 
     //获取天气数据
     $.get('https://jirenguapi.applinzi.com/weather.php')
-     .done(function(weatheData) {
-         console.log(JSON.parse(weatheData))
+     .done(function(data) {
+         let weatherData =JSON.parse(data).results[0].weather_data//4个对象
+         console.log(weatherData)
+         weatherData.forEach(function(item,index) {
+             $('#main-content>li').eq(index).children().eq(0).text('0' + (nowDay.getMonth() + 1) + '月' + (nowDay.getDate() +index) + '日')
+             $('#main-content>li').eq(index).children().eq(1).text(item.temperature)
+             if(/[\u4e91]/.test(item.weather)) {
+                 $('#main-content>li').eq(index).children().eq(2).children().attr('xlink:href','#icon-icon-test1')
+             } else if(/[\u96e8]/.test(item.weather)) {
+                 $('#main-content>li').eq(index).children().eq(2).children().attr('xlink:href','#icon-icon-test4')
+             } else {
+                 $('#main-content>li').eq(index).children().eq(2).children().attr('xlink:href','#icon-sun')
+             }
+         })
      })
+
+
+
+
+    //展示信息
+
 
 })
 
