940e7480f91cf0e4cefddb4dd2db29c44691a90f
leo <leozhang621@gmail.com>
2017-07-16 17:50:32 +0800

    update carousel

---
+++ carousel-fade/carousel.js
@@ -0,0 +1,107 @@
+let $imageCarousel = $('#carousel')
+let $images = $('#carousel>li')
+let imagesNum = $images.length
+let $navs = $('#nav>li')
+let timeId
+let $nextButton = $('.button-next')
+let $preButton = $('.button-pre')
+
+
+
+let inPage = 0     //重要的两个参数：进入页面和离开页面
+let outPage = 0
+let canClick = true
+
+
+
+
+nextFade()
+autoFade()
+
+
+//自动轮播
+function  autoFade() {
+    timeId = setInterval(function() {
+        nextFade()
+    },3000)
+}
+
+
+//下一页逻辑
+function nextFade(len) {
+    if(!canClick) {
+        return
+    }
+    canClick = false
+    if(len) {
+        inPage += len - 1
+    }
+    if(inPage > imagesNum - 1) {
+        inPage = 0
+    }
+    $images.eq(outPage).fadeOut(1000)
+    $images.eq(inPage).fadeIn(1000,function() {
+        canClick = true
+    })
+    setNav(inPage)
+    outPage = inPage    //传递下一轮消失页面
+    inPage++
+}
+
+//上一页逻辑
+function preFade(len) {
+    if(!canClick) {
+        return
+    }
+    canClick = false
+    if(len) {
+        inPage -= len + 1
+    } else {
+        inPage -= 2
+    }
+    if(inPage < 0) {
+        inPage = imagesNum - 1
+    }
+    $images.eq(outPage).fadeOut(1000)
+    $images.eq(inPage).fadeIn(1000,function() {
+        canClick = true
+    })
+    setNav(inPage)
+    outPage = inPage    //传递下一轮消失页面
+    inPage++
+}
+
+
+//导航条设置
+function setNav(index) {
+    $navs.eq(index).addClass('active')
+    $navs.eq(index).siblings().removeClass('active')
+}
+
+
+//next事件
+$nextButton.on('click',function() {
+    window.clearInterval(timeId)
+    nextFade()
+    autoFade()
+})
+
+
+//pre事件
+$preButton.on('click',function() {
+    window.clearInterval(timeId)
+    preFade()
+    autoFade()
+})
+
+//nav事件
+$navs.on('click',function(){
+    window.clearInterval(timeId)
+    var clickIndex = $(this).index()
+    if(clickIndex > outPage) {
+        nextFade(clickIndex - outPage)
+    } else if (clickIndex < outPage) {
+        preFade(outPage - clickIndex)
+    }
+    autoFade()
+})
---
+++ carousel-slide/carousel.js
@@ -0,0 +1,123 @@
+
+
+let $imagesCarousel = $('#carousel');
+let $images = $('#carousel>li')
+let imagesNum = $images.length          //图片数量
+let imageSize = $images.width()
+let $navs = $('#nav>li')
+let index = 0
+let canClick = true
+let timeId
+
+let $preButton = $('.button-pre')
+let $nextButton = $('.button-next')
+
+
+//首尾图片插入
+$images.first().clone().appendTo($imagesCarousel)
+$images.last().clone().prependTo($imagesCarousel)
+$imagesCarousel.css({left: -imageSize})
+
+
+autoPlay()
+
+//自动播放
+function autoPlay() {
+    timeId =  setInterval(function() {
+        next()
+    },2500)
+}
+
+//上一页事件
+$preButton.on('click',function(e) {
+    window.clearInterval(timeId)
+    if(!canClick){
+        return
+    }
+    canClick = false
+    e.preventDefault()
+    pre()
+    autoPlay()
+})
+
+
+//下一页事件
+$nextButton.on('click',function(e) {
+    window.clearInterval(timeId)
+    if(!canClick) {
+        return
+    }
+    canClick =false
+    e.preventDefault()
+    next()
+    autoPlay()
+})
+
+
+//nav事件
+$navs.on('click',function(e) {
+    window.clearInterval(timeId)
+    if(!canClick) {
+        return
+    }
+    canClick = false
+    e.preventDefault()
+    let page = $(this).index()
+
+    //方法1
+    // $imagesCarousel.animate({left: -imageSize*(page + 1)},1000,function() {
+    //     canClick = true
+    //     setNav(page)
+    // })
+
+
+    //方法2
+    if(page > index) {
+        next(page - index)
+        autoPlay()
+    } else if(page < index) {
+        pre(index -page)
+        autoPlay()
+    }
+
+})
+
+
+//上一页逻辑
+function pre(length) {
+    if(length === undefined) {
+        length = 1
+    }
+    $imagesCarousel.animate({left: "+=" + imageSize*length},1000,function() {
+        canClick = true
+        index -= length
+        if(index < 0) {
+            $imagesCarousel.css({left: -imageSize*imagesNum})
+            index = imagesNum - 1
+        }
+        setNav(index)
+    })
+}
+
+
+//下一页逻辑
+function next(length) {
+    if(length === undefined) {
+        length = 1
+    }
+    $imagesCarousel.animate({left: "-=" + imageSize*length},1000,function() {
+        canClick = true
+        index += length
+        if(index === imagesNum) {
+            $imagesCarousel.css({left: -imageSize})
+            index = 0
+        }
+        setNav(index)
+    })
+}
+
+//导航条设置
+function setNav(index) {
+    $navs.eq(index).toggleClass('active')
+    $navs.eq(index).siblings().removeClass('active')
+}
