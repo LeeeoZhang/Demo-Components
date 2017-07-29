5ef8bb067b9a502e7808d8198fc47b289dc2a5dd
leo <leozhang621@gmail.com>
2017-07-29 21:43:47 +0800

    update news_waterfall

--- news_waterfall/news_waterfall.js
+++ news_waterfall/news_waterfall.js
@@ -1,87 +1,93 @@
-
-
-
-let $container = $('#waterfall')
-let $load = $('#load')
-let num = 8
-let page = 0
-let columnHeights = [0,0,0]
-let canScroll = true
-
-//获取数据
-function getDate(callback) {
-    if(!canScroll) return
-    canScroll = false       //上锁，防止获取重复数据
-    let url =  `https://platform.sina.com.cn/slide/album_tech?app_key=1271687855&num=${num}&page=${page}`
-    $.ajax({
-        url: url,
-        method: 'get',
-        dataType: 'jsonp',
-        jsonp: 'jsoncallback'
-    }).done((response) => {
-        callback(response.data)    //获取数据成功执行回调函数
-    }).fail(() => {
-        alert('获取新闻出错')
-    })
-}
-
-
-//处理数据
-function dealDate(data) {
-    data.forEach((item,index) => {
-        let $node = makeNode(item)
-        $node.find('img').on('load',function() {   //当图片加载完毕才把节点放入页面，这样才能获取元素高度
-            $container.append($node)
-            waterFall($node)
-            page++                      //完成一页数据后，获取下一页
-            canScroll = true            //解锁
-        })
-        $node.find('img').attr('src',item.img_url)   //设置回调函数在src赋值前，保证回调函数可以执行
-    })
-}
-
-
-//拼接节点
-function makeNode(nodeDate) {
-    let $div = $('<div class="new"></div>')
-    let $a = $(`<a href=${nodeDate.url}></a>`)
-    $(`<img  alt="">`).appendTo($a)
-    $(`<h4>${nodeDate.short_name}</h4>`).appendTo($a)
-    $(`<p>${nodeDate.short_intro}</p>`).appendTo($a)
-    $div.append($a)
-    return $div
-}
-
-//瀑布流逻辑
-function waterFall($node) {
-    let nodeHeight = $node.outerHeight(true)
-    let nodeWidth = $node.outerWidth(true)
-    let minColumHeight = Math.min.apply(null,columnHeights)
-    let minColumnHeightIndex = columnHeights.indexOf(minColumHeight)
-    $node.css({
-        left: minColumnHeightIndex*nodeWidth,
-        top: minColumHeight
-    })
-    columnHeights[minColumnHeightIndex] += nodeHeight
-    $container.css({
-        height: Math.max.apply(null,columnHeights)
-    })
-}
-
-//隐藏节点是否出现在视野
-function isLoad() {
-    return ($(window).scrollTop() + $(window).height()) + 200 > $load.offset().top
-}
-
-
-getDate(dealDate)
-
-//滚动加载新闻
-$(window).on('scroll',() => {
-    if(isLoad()) {
-        getDate(dealDate)
+let Explosure = (function() {
+    function _Explosure(waterFallCt,target,callback) {
+        this.waterFallCt = waterFallCt
+        this.target = target
+        this.callback = callback
+        this.canScroll = true
+        this.num = 8
+        this.page = 0
+        this.getData()
+        this.bind()
+        this.columnHeights = [0,0,0]
     }
+    _Explosure.prototype.getData = function() {
+        if(!this.canScroll) return
+        this.canScroll = false       //上锁，防止获取重复数据
+        let url =  `https://platform.sina.com.cn/slide/album_tech?app_key=1271687855&num=${this.num}&page=${this.page}`
+        $.ajax({
+            url: url,
+            method: 'get',
+            dataType: 'jsonp',
+            jsonp: 'jsoncallback'
+        }).done((response) => {
+            this.callback(response.data)    //获取数据成功执行回调函数
+        }).fail(() => {
+            alert('获取新闻出错')
+        })
+    }
+    _Explosure.prototype.isShow = function() {
+        return (window.innerHeight + window.scrollY) + 200 > target.offsetTop
+    }
+    _Explosure.prototype.bind = function() {
+        let _this = this
+        window.addEventListener('scroll',function() {
+            if(_this.isShow()) {
+                _this.getData()
+            }
+        })
+    }
+    _Explosure.prototype.makeNode = function(nodeData) {
+        let div = document.createElement('div')
+        let a = document.createElement('a')
+        let img = document.createElement('img')
+        let h4 = document.createElement('h4')
+        let p = document.createElement('p')
+        div.classList.add('new')
+        a.setAttribute('href',nodeData.url)
+        h4.innerText = nodeData.short_name
+        p.innerText = nodeData.short_intro
+        a.appendChild(img)
+        a.appendChild(h4)
+        a.appendChild(p)
+        div.appendChild(a)
+        return div
+    }
+    _Explosure.prototype.waterFall = function(node) {
+        let styles = window.getComputedStyle(node);
+        let marginHeight = parseFloat(styles['marginTop']) + parseFloat(styles['marginBottom'])
+        let marginWidth = parseFloat(styles['marginTop']) + parseFloat(styles['marginBottom'])
+        let nodeHeight = Math.ceil(node.offsetHeight + marginHeight)
+        console.log(nodeHeight)
+        let nodeWidth = Math.ceil(node.offsetWidth + marginWidth)
+        console.log(nodeWidth)
+        let minColumnHeight = Math.min.apply(null,this.columnHeights)
+        let minColumnHeightIndex = this.columnHeights.indexOf(minColumnHeight)
+        node.style.left = `${minColumnHeightIndex * nodeWidth}px`
+        node.style.top = `${minColumnHeight}px`
+        this.columnHeights[minColumnHeightIndex] += nodeHeight
+        this.waterFallCt.style.height = `${Math.max.apply(null,this.columnHeights)}px`
+    }
+    return {
+        init: function(waterFallCt,target,callback) {
+            new _Explosure(waterFallCt,target,callback)
+        }
+    }
+})()
+
+let waterFallCt = document.querySelector('#waterfall'),
+    target = document.querySelector('#load')
+
+Explosure.init(waterFallCt,target,function(data){
+    let _this = this
+    data.forEach((item,index) => {
+        let node = this.makeNode(item)
+        let imgNode = node.querySelector('img')
+        imgNode.addEventListener('load',function() {
+            _this.waterFallCt.appendChild(node)
+            _this.waterFall(node)
+            _this.page++
+            _this.canScroll = true
+        })
+        imgNode.setAttribute('src',item.img_url)
+    })
 })
-
-
-
