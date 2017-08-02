6dababe015792e16131f7889c2f9ec3bdb7eac22
leo <leozhang621@gmail.com>
2017-08-02 20:41:05 +0800

    update master

--- The Grandmaster/js/GoTop.js
+++ The Grandmaster/js/GoTop.js
@@ -10,13 +10,13 @@ $(window).on('scroll',function() {
         return
     }
     canScroll = false
-    if($(window).scrollTop() > 400) {
+    if($(window).scrollTop() > 1000) {
         $nav.animate({padding: 0,
                       opacity: 1},function() {
             canScroll = true
         })
     }
-    if($(window).scrollTop() < 400) {
+    if($(window).scrollTop() < 1000) {
         $nav.animate({padding: '25px 0',
                       opacity: .5},function() {
             canScroll = true
