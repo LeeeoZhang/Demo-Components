06f7db90aa4709a1e1ab17e6d262cee385ff576e
leo <leozhang621@gmail.com>
2017-08-07 23:54:58 +0800

    update

--- weather_npm/main.js
+++ weather_npm/main.js
@@ -1,5 +1,23 @@
-// !# /usr/bin/env node
+#! /usr/bin/env node
 let request = require('request')
-request('https://weixin.jirengu.com/weather',function(error,response, body) {
-	console.log(body)
-})
+if(process.argv[2]) {
+	request(`https://weixin.jirengu.com/weather/cityid?location=${process.argv[2]}`,function(error,response, body) {
+		let cityId = JSON.parse(body).results[0].id
+		let url = `https://weixin.jirengu.com/weather/now?cityid=${cityId}`
+		request(url,function(error,response, body) {
+			let data = JSON.parse(body)
+			console.log(data.weather[0].city_name)
+			console.log(`今天气温${data.weather[0].future[0].low}℃~${data.weather[0].future[0].high}℃ ${data.weather[0].future[0].text}`)
+			console.log(`明天气温${data.weather[0].future[1].low}℃~${data.weather[0].future[1].high}℃ ${data.weather[0].future[1].text}`)
+		})
+
+	})
+} else {
+	request('https://weixin.jirengu.com/weather',function(error,response, body) {
+		let data = JSON.parse(body)
+		console.log(data.weather[0].city_name)
+		console.log(`今天气温${data.weather[0].future[0].low}℃~${data.weather[0].future[0].high}℃ ${data.weather[0].future[0].text}`)
+		console.log(`明天气温${data.weather[0].future[1].low}℃~${data.weather[0].future[1].high}℃ ${data.weather[0].future[1].text}`)
+	})
+}
+
---
+++ weather_npm/readme.md
@@ -0,0 +1,20 @@
+## Install
+```bash
+npm i -g weather_leo
+```
+
+## Using
+1.可以直接输入`tianqi`或者`weather`查看当前城市的天气情况
+```
+tianqi
+```
+```
+weather
+```
+2.也可以像下面这样，查看某个城市的天气
+```
+tianqi beijing
+```
+```
+weather beijing
+```
