ac1573767025a1e521137bc46f34b6e2a41f993e
leo <leozhang621@gmail.com>
2017-08-11 23:40:20 +0800

    update weather_npm

--- weather_npm/main.js
+++ weather_npm/main.js
@@ -6,18 +6,22 @@ if(process.argv[2]) {
 		let url = `https://weixin.jirengu.com/weather/now?cityid=${cityId}`
 		request(url,function(error,response, body) {
 			let data = JSON.parse(body)
+			console.log('---------------------------')
 			console.log(data.weather[0].city_name)
 			console.log(`今天气温${data.weather[0].future[0].low}℃~${data.weather[0].future[0].high}℃ ${data.weather[0].future[0].text}`)
 			console.log(`明天气温${data.weather[0].future[1].low}℃~${data.weather[0].future[1].high}℃ ${data.weather[0].future[1].text}`)
+			console.log('---------------------------')
 		})
 
 	})
 } else {
 	request('https://weixin.jirengu.com/weather',function(error,response, body) {
 		let data = JSON.parse(body)
+		console.log('---------------------------')
 		console.log(data.weather[0].city_name)
 		console.log(`今天气温${data.weather[0].future[0].low}℃~${data.weather[0].future[0].high}℃ ${data.weather[0].future[0].text}`)
 		console.log(`明天气温${data.weather[0].future[1].low}℃~${data.weather[0].future[1].high}℃ ${data.weather[0].future[1].text}`)
+		console.log('---------------------------')
 	})
 }
 
