ed5980b018fe9974ca3ae48ec5d5c1d380676f1a
leo <leozhang621@gmail.com>
2017-10-20 20:11:18 +0800

    test

--- The Grandmaster/webpack.config.js
+++ The Grandmaster/webpack.config.js
@@ -6,26 +6,23 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-  	rules: [
-  		{
-  			test: /\.css$/,
-  			use: [
-  				'style-loader',
-  				'css-loader',
-  				'autoprefixer-loader'
-  			]
-  		},
-  		{
-  			test: /\.(png|woff|woff2|eot|ttf|svg|otf)$/,
-  			use: [
-  				'url-loader?imit=100000'
-  			]
-  		},
-      {
-        test: /\.js[x]?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader?presets[]=es2015'
-      }
-  	]
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        'sass-loader',
+        'style-loader',
+        'css-loader',
+        'autoprefixer-loader'
+      ]
+    }, {
+      test: /\.(png|woff|woff2|eot|ttf|svg|otf)$/,
+      use: [
+        'url-loader?imit=100000'
+      ]
+    }, {
+      test: /\.js[x]?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader?presets[]=es2015'
+    }]
   }
 };
